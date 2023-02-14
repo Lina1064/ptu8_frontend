9e2722ddb66acd4b0fd9397e30a56359663cfebc
Lina1064 <lina.vigricaite@gmail.com>
2023-02-14 09:59:45 +0200

    likusi_dalis

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ def index():
 def destytojas():
     return render_template("destytojas.html")
 
+@app.route('/lina/')
+def destytojas():
+    return render_template("lina.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
